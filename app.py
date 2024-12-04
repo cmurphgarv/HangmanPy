e6bd40c15ea4a6cb514fbf6e3b0bd14e9863b8d7
Charlotte Murphey <murphey.charlotte@gmail.com>
2024-12-04 11:34:01 -0500

    added word list to randomly choose from for game

---
+++ app.py
@@ -0,0 +1,15 @@
+import random
+from words import words_list
+
+# list of words, pull from randomly and select a word
+print(words_list)
+
+# number of guesses limited by hangman parts
+
+# visually show how many letters in word
+
+# user gets feedback for each guess (correct or wrong)
+
+# game ends when hangman is complete or they finish the word correctly
+
+# prompt to play again or terminate
