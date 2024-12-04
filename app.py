d3d413c25d0b24a54239f3877f2e468edc659d6f
Charlotte Murphey <murphey.charlotte@gmail.com>
2024-12-04 11:58:14 -0500

    began adding guess limits and visuals for word to guess

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import random
 from words import words_list
 
 # list of words, pull from randomly and select a word
-print(words_list)
+word_to_guess = random.choice(words_list)
+print(word_to_guess)
 
 # number of guesses limited by hangman parts
+guess_limit = 6
 
 # visually show how many letters in word
+underscores_num = len(word_to_guess)
+print('_ ' * underscores_num)
 
 # user gets feedback for each guess (correct or wrong)
 
