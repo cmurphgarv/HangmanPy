2ca0f52968d36a00201863f754e0d138720a2f02
Charlotte Murphey <murphey.charlotte@gmail.com>
2024-12-05 12:44:50 -0500

    added new function for printing progress in guessing

--- app.py
+++ app.py
@@ -3,14 +3,22 @@ from words import words_list
 
 # list of words, pull from randomly and select a word
 word_to_guess = random.choice(words_list)
-print(word_to_guess)
+
 
 # number of guesses limited by hangman parts
+# head, 2 arms, 2 legs, body
 guess_limit = 6
 
-# visually show how many letters in word
-underscores_num = len(word_to_guess)
-print('_ ' * underscores_num)
+# visually show how many letters in word with underscores
+# fill in as they guess correct letters and reprint
+def print_word_progress():
+    underscores_num = len(word_to_guess)
+    print('_ ' * underscores_num)
+    correct_letters = word_to_guess.split()
+    print(correct_letters)    
+
+def playHangman():
+    while True:
 
 # user gets feedback for each guess (correct or wrong)
 
